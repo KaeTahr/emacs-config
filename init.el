17fafa7fe38cf9313ba023927e41124e8387390f
kaetahr <a00825945@itesm.mx>
2019-12-28 22:07:42 -0600

    added autoupdate

--- init.el
+++ init.el
@@ -86,7 +86,11 @@
   (evil-mode 1)
   :config
   (evil-set-initial-state 'term-mode 'emacs)
-  (evil-set-initial-state 'dired-mode 'emacs))
+  (evil-set-initial-state 'dired-mode 'emacs)
+  (evil-set-initial-state 'eshell-mode 'emacs)
+  (evil-set-initial-state 'gdb 'emacs)
+  (evil-set-initial-state 'equake-mode 'emacs))
+
 
 ;; switching windows
 (use-package switch-window
@@ -250,6 +254,12 @@
   :ensure t
   :config
   (global-origami-mode))
+
+;; update emacs packages
+(use-package auto-package-update
+  :ensure t
+  :init
+  (auto-package-update-maybe))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; End of packages ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; display columns
@@ -297,6 +307,12 @@
 ;; Make sure syntax highliting is on
 (global-font-lock-mode 1)
 (setq python-shell-enable-font-lock nil) ; if you don't do this then python shell is unbearably slow
+
+
+;; Cleaning up shell mode
+(add-hook 'eshell-mode 'beacon-mode)
+(add-hook 'equake-mode 'beacon-mode)
+(add-hook 'equake-mode 'column-number-mode)
 
 ;; Init screen and dashboard configuration:
 ;; Don't display default init screen
@@ -411,14 +427,14 @@
    (quote
     ("bbef8cbdabf3b084dd01e548e064a1c87e857e2332a8defdf85520ba2b4fc6f1" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "86704574d397606ee1433af037c46611fb0a2787e8b6fd1d6c96361575be72d2" "3cd4f09a44fe31e6dd65af9eb1f10dc00d5c2f1db31a427713a1784d7db7fdfc" default)))
  '(equake-default-sh-command "/bin/bash")
- '(equake-default-shell (quote ansi-term))
+ '(equake-default-shell (quote eshell))
  '(equake-opacity-active 95)
  '(equake-opacity-inactive 90)
  '(equake-size-height 1.0)
  '(gdb-many-windows t)
  '(package-selected-packages
    (quote
-    (origami pydoc-info htmlize company-irony equake aggressive-indent agressive-indent elpy-company elpy py-autopep8 flycheck yasnippet-snippets yasnippet expand-region mark-multiple popup-kill-ring company hungry-delete evil rainbow-mode avy smex org-bullets try beacon xresources-theme nyx-theme which-key use-package))))
+    (auto-package-update origami pydoc-info htmlize company-irony equake aggressive-indent agressive-indent elpy-company elpy py-autopep8 flycheck yasnippet-snippets yasnippet expand-region mark-multiple popup-kill-ring company hungry-delete evil rainbow-mode avy smex org-bullets try beacon xresources-theme nyx-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
